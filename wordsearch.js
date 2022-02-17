ccd8c366e5ad87c89cab4d960755f54fb8c797dc
Keila Kopvillem <kmkopvillem@gmail.com>
2022-02-16 20:33:24 -0500

    wordSearch function using transpose function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+    let newMatrix = [];
+    for (let i = 0; i < cols; i++) {
+      newMatrix[i] = Array(rows);
     }
-}
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        newMatrix[j][i] = matrix[i][j];
+      }
+    }
+    return newMatrix;
+  };
+  
+  const isWordInMatrix = (joinedLetters, word) => {
+    for (let i = 0; i < joinedLetters.length; i++) {
+      if (joinedLetters[i].includes(word)) return true;
+    }
+    return false;
+  };
+  
+  const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const vertical = transpose(letters);
+    const verticalJoin = vertical.map(ls => ls.join(''));
+    return isWordInMatrix(horizontalJoin, word) || isWordInMatrix(verticalJoin, word);
+  };
 
-module.exports = wordSearch
+
+  module.exports = wordSearch
+
+  
